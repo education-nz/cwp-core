ee2b8cbc0aaa7262242a182beb627dbd8f802deb
Will Rossiter <will@fullscreen.io>
2018-10-01 10:23:40 +1300

    Patch module, incorporate into ours

---
+++ src/Middleware/TrailingSlashRedirector.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Education\Cwp\Middleware;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Control\Middleware\HTTPMiddleware;
+
+/**
+ * Ensure that a single trailing slash is always added to the URL.
+ * URLs accessed via Ajax, contain $_GET vars, or that contain
+ * an extension are ignored.
+ */
+class TrailingSlashRedirector implements HTTPMiddleware
+{
+    public function process(HTTPRequest $request, callable $delegate)
+    {
+        if ($request && ($request->isGET() || $request->isHEAD())) {
+            $requested_url = $_SERVER['REQUEST_URI'];
+            $expected_url = rtrim(Director::baseURL() . $request->getURL(), '/') . '/';
+            $urlPathInfo = pathinfo($requested_url);
+            $params = $request->getVars();
+
+            if (isset($params['url'])) {
+                unset($params['url']);
+            }
+
+            if (!Director::is_ajax() &&
+                !isset($urlPathInfo['extension']) &&
+                empty($params) &&
+                !preg_match('/^' . preg_quote($expected_url, '/') . '(?!\/)/i', $requested_url)
+            ) {
+                $params = $request->getVars();
+                $redirect_url = Controller::join_links($expected_url, '/');
+                $response = new HTTPResponse();
+
+                return $response->redirect($redirect_url, 301);
+            }
+        }
+
+        $response = $delegate($request);
+
+        return $response;
+    }
+}
