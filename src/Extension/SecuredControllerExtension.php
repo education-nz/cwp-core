97303a0f15018167cdbbde7c95b0ff60ad64bdc5
Will Rossiter <will@fullscreen.io>
2020-11-11 14:34:35 +1300

    EVAL-79, EVAL-81 Fix S3 images being blocked, clean up rules, allow data fonts

--- src/Extension/SecuredControllerExtension.php
+++ src/Extension/SecuredControllerExtension.php
@@ -3,8 +3,25 @@
 namespace Education\Cwp\Extension;
 
 use SilverStripe\Control\Director;
+use SilverStripe\Core\Environment;
 use SilverStripe\Core\Extension;
 
+/**
+ * Adds the relevant CSP records to all controller actions.
+ *
+ * To add or modify these records on a per project basis, use the
+ * `updateExtraHeaders` method on your PageController class or an extension
+ * of ContentController and str_replace.
+ *
+ * <code>
+ * public function updateExtraHeaders(&$csp)
+ * {
+ *  $csp = str_replace('script-src ', 'script-src https://www.hostname.com ', $csp);
+ *
+ *  return $csp;
+ * }
+ * </code>
+ */
 class SecuredControllerExtension extends Extension
 {
     public function onAfterInit()
@@ -17,7 +34,7 @@ class SecuredControllerExtension extends Extension
         $response = $this->owner->getResponse();
 
         if ($response && $this->owner->supportsCSP()) {
-            $knownDomains = 'https://*.education.govt.nz https://*.cwp.govt.nz';
+            $knownDomains = 'https://*.education.govt.nz';
 
             $base = rtrim(Director::absoluteBaseURL(), '/');
             if (strpos($knownDomains, $base) === false) {
@@ -27,6 +44,18 @@ class SecuredControllerExtension extends Extension
             $baseWithSlash = rtrim(Director::absoluteBaseURL(), '/') . '/';
             if (strpos($knownDomains, $baseWithSlash) === false) {
                 $knownDomains .= ' '. $baseWithSlash;
+            }
+
+            // static base url
+            if ($base = Environment::getEnv('SS_STATIC_BASE_URL')) {
+                $knownDomains .= ' '. $base;
+            }
+
+            // S3 bucket if needed
+            if ($s3 = Environment::getEnv('AWS_BUCKET_NAME')) {
+                $s3path = sprintf('https://%s.s3.%s.amazonaws.com', $s3, Environment::getEnv('AWS_REGION'));
+
+                $knownDomains .= ' '. $s3path;
             }
 
             // if the base URL is using https then allow http as a version to
@@ -41,14 +70,14 @@ class SecuredControllerExtension extends Extension
             $csp = "default-src $knownDomains www.google.com www.gstatic.com 'unsafe-inline' 'unsafe-eval' data:;";
             $csp .= " base-uri $knownDomains;";
             $csp .= " frame-ancestors $knownDomains;";
-            $csp .= " style-src 'unsafe-inline' $knownDomains https://*.typography.com https://maxcdn.bootstrapcdn.com https://*.fontawesome.com https://fonts.googleapis.com https://*.gstatic.com https://api.addressfinder.io https://tagmanager.google.com https://optimize.google.com;";
-            $csp .= " script-src 'unsafe-inline' $knownDomains https://player.vimeo.com https://script.crazyegg.com https://code.jquery.com https://staticcdn.co.nz https://snap.licdn.com https://www.google.com https://*.doubleclick.net https://www.googleadservices.com https://*.fontawesome.com https://connect.facebook.net http://script.crazyegg.com https://*.crazyegg.com https://*.gstatic.com https://api.addressfinder.io https://www.googletagmanager.com https://fonts.googleapis.com https://*.google-analytics.com http://*.google-analytics.com http://tagmanager.google.com https://optimize.google.com http://*.hotjar.com https://*.hotjar.com https://code.jquery.com 'unsafe-eval';";
-            $csp .= " img-src $knownDomainsInsecure 'self' data: https://googleads.g.doubleclick.net https://stats.g.doubleclick.net https://*.amazonaws.com https://www.google.co.nz https://p.adsymptotic.com https://www.google.com https://px.ads.linkedin.com https://*.facebook.com https://*.google-analytics.com http://*.google-analytics.com https://*.swagger.io https://optimize.google.com https://*.hotjar.com;";
-            $csp .= " font-src $knownDomains https://*.fontawesome.com https://*.typography.com https://maxcdn.bootstrapcdn.com https://fonts.gstatic.com http://*.hotjar.com https://*.hotjar.com;";
+            $csp .= " style-src 'unsafe-inline' $knownDomains https://*.typography.com https://maxcdn.bootstrapcdn.com https://*.fontawesome.com https://fonts.googleapis.com https://*.gstatic.com https://tagmanager.google.com https://optimize.google.com;";
+            $csp .= " script-src 'unsafe-inline' $knownDomains https://player.vimeo.com https://code.jquery.com https://staticcdn.co.nz https://snap.licdn.com https://www.google.com https://*.doubleclick.net https://www.googleadservices.com https://*.fontawesome.com https://connect.facebook.net https://*.gstatic.com https://www.googletagmanager.com https://fonts.googleapis.com https://*.google-analytics.com http://*.google-analytics.com http://tagmanager.google.com https://optimize.google.com https://code.jquery.com 'unsafe-eval';";
+            $csp .= " img-src $knownDomainsInsecure 'self' data: https://googleads.g.doubleclick.net https://stats.g.doubleclick.net https://www.google.co.nz https://p.adsymptotic.com https://www.google.com https://px.ads.linkedin.com https://*.facebook.com https://*.google-analytics.com http://*.google-analytics.com https://*.swagger.io https://optimize.google.com;";
+            $csp .= " font-src $knownDomains data: https://*.fontawesome.com https://*.typography.com https://maxcdn.bootstrapcdn.com https://fonts.gstatic.com;";
             $csp .= " object-src $knownDomains 'self';";
             $csp .= " frame-src $knownDomainsInsecure 'self' https://www.facebook.com https://bid.g.doubleclick.net https://www.google.com data: https://*.youtube-nocookie.com https://player.vimeo.com https://*.youtube.com https://optimize.google.com https://www.googletagmanager.com/ns.html https://*.hotjar.com;";
             $csp .= " child-src $knownDomains https://*.youtube-nocookie.com https://player.vimeo.com http://player.vimeo.com https://*.youtube.com  https://optimize.google.com https://www.googletagmanager.com/ns.html https://*.hotjar.com;";
-            $csp .= " connect-src $knownDomains https://api.addressfinder.io https://www.google-analytics.com/ http://*.hotjar.com:* https://*.hotjar.com:* ws://*.hotjar.com wss://*.hotjar.com;";
+            $csp .= " connect-src $knownDomains https://www.google-analytics.com/;";
             $csp .= " form-action $knownDomainsInsecure https://www.facebook.com 'self';";
             // phpcs:enable
 
