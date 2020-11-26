9c0b8f62d5f9120130901f57daccedbe00272087
Will Rossiter <will@fullscreen.io>
2020-11-26 15:40:13 +1300

    Alt tags not required on files

--- src/Extension/ImageFormFactoryExtension.php
+++ src/Extension/ImageFormFactoryExtension.php
@@ -2,19 +2,25 @@
 
 namespace Education\Cwp\Extension;
 
+use SilverStripe\Assets\Image;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 
 class ImageFormFactoryExtension extends DataExtension
 {
-    public function updateFormFields(FieldList $fields)
+    public function updateFormFields(FieldList $fields, $controller, $name, $context)
     {
         $fields->dataFieldByName('Title')->setDescription('Not displayed, for images with captions use alt fields');
 
-        $fields->insertAfter(
-            'Title',
-            TextField::create('AltText', 'Alt Text')->setMaxLength(255)
-        );
+        /** @var File $record */
+        $record = $context['Record'];
+
+        if ($record && $record instanceof Image) {
+            $fields->insertAfter(
+                'Title',
+                TextField::create('AltText', 'Alt Text')->setMaxLength(255)
+            );
+        }
     }
 }
