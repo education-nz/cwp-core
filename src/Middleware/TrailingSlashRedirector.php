3f0a032d1ec63f4b03e0bddcc2d96b9159188f38
Will Rossiter <will@fullscreen.io>
2021-01-18 11:23:20 +1300

    Ignore some paths for trailing slash

--- src/Middleware/TrailingSlashRedirector.php
+++ src/Middleware/TrailingSlashRedirector.php
@@ -2,12 +2,12 @@
 
 namespace Education\Cwp\Middleware;
 
-use SilverStripe\Admin\AdminRootController;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Control\Middleware\HTTPMiddleware;
+use SilverStripe\Core\Config\Config;
 
 /**
  * Ensure that a single trailing slash is always added to the URL.
@@ -16,37 +16,87 @@ use SilverStripe\Control\Middleware\HTTPMiddleware;
  */
 class TrailingSlashRedirector implements HTTPMiddleware
 {
+    /**
+     * URLS to ignore
+     *
+     * @var array
+     */
+    private static $ignore_paths = [
+        'admin/',
+        'dev/',
+    ];
+
+    /**
+     * User-Agents to ignore
+     *
+     * @var array
+     */
+    private static $ignore_agents = [
+        'silverstripe/staticpublishqueue',
+    ];
+
+    /**
+     * Process request
+     *
+     * @param HTTPRequest $request  HTTP request
+     * @param callable    $delegate Delegate
+     *
+     * @return HTTPResponse
+     */
     public function process(HTTPRequest $request, callable $delegate)
     {
+        $ignore_paths = [];
+
+        $ignore_config = Config::inst()
+            ->get(TrailingSlashRedirector::class, 'ignore_paths');
+
+        foreach ($ignore_config as $iurl) {
+            if ($quoted = preg_quote(ltrim($iurl, '/'), '/')) {
+                array_push($ignore_paths, $quoted);
+            }
+        }
+
         if ($request && ($request->isGET() || $request->isHEAD())) {
-            $requestedUrl = $_SERVER['REQUEST_URI'];
-
-            // don't process any requested admin URL
-            if (strpos($requestedUrl, AdminRootController::admin_url()) !== false) {
-                $response = $delegate($request);
-
-                return $response;
+            // skip $ignore_paths and home (`/`)
+            if ($request->getURL() == ''
+                || preg_match(
+                    '/^(' . implode('|', $ignore_paths) . ')/i',
+                    $request->getURL()
+                )
+            ) {
+                return $delegate($request);
             }
 
-            $expectedUrl = rtrim(Director::baseURL() . $request->getURL(), '/') . '/';
-            $urlPathInfo = pathinfo($requestedUrl);
+            $requested_url = $_SERVER['REQUEST_URI'];
+
+            $expected_url = rtrim(
+                Director::baseURL() . $request->getURL(), '/'
+            ) . '/';
+
+            $urlPathInfo = pathinfo($requested_url);
+
             $params = $request->getVars();
 
-            if (isset($params['url'])) {
-                unset($params['url']);
-            }
+            $ignore_agents = Config::inst()
+                ->get(TrailingSlashRedirector::class, 'ignore_agents');
 
-            if (!Director::is_ajax() &&
-                !Director::is_cli() &&
-                !isset($urlPathInfo['extension']) &&
-                empty($params) &&
-                !preg_match('/^' . preg_quote($expectedUrl, '/') . '(?!\/)/i', $requestedUrl)
+            if (!Director::is_ajax()
+                && !($ignore_agents && in_array(
+                    $request->getHeader('User-Agent'),
+                    $ignore_agents
+                ))
+                && !isset($urlPathInfo['extension'])
+                && empty($params)
+                && !preg_match(
+                    '/^' . preg_quote($expected_url, '/') . '(?!\/)/i',
+                    $requested_url
+                )
             ) {
-                $params = $request->getVars();
-                $redirectUrl = Controller::join_links($expectedUrl, '/');
-                $response = new HTTPResponse();
+                $params       = $request->getVars();
+                $redirect_url = Controller::join_links($expected_url, '/');
+                $response     = new HTTPResponse();
 
-                return $response->redirect($redirectUrl, 301);
+                return $response->redirect($redirect_url, 301);
             }
         }
 
