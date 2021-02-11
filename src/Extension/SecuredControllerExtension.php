ca46d614b39ae6e2533b8341c00e8d33e580eaa2
Will Rossiter <will@fullscreen.io>
2021-02-11 14:40:10 +1300

    Add alogolia to csp

--- src/Extension/SecuredControllerExtension.php
+++ src/Extension/SecuredControllerExtension.php
@@ -72,12 +72,12 @@ class SecuredControllerExtension extends Extension
             $csp .= " frame-ancestors $knownDomains;";
             $csp .= " style-src 'unsafe-inline' $knownDomains https://*.typography.com https://maxcdn.bootstrapcdn.com https://*.fontawesome.com https://fonts.googleapis.com https://*.gstatic.com https://tagmanager.google.com https://optimize.google.com;";
             $csp .= " script-src 'unsafe-inline' $knownDomains https://player.vimeo.com https://code.jquery.com https://staticcdn.co.nz https://snap.licdn.com https://www.google.com https://*.doubleclick.net https://www.googleadservices.com https://*.fontawesome.com https://connect.facebook.net https://*.gstatic.com https://www.googletagmanager.com https://fonts.googleapis.com https://*.google-analytics.com http://*.google-analytics.com http://tagmanager.google.com https://optimize.google.com https://code.jquery.com 'unsafe-eval';";
-            $csp .= " img-src $knownDomainsInsecure 'self' data: https://googleads.g.doubleclick.net https://stats.g.doubleclick.net https://www.google.co.nz https://p.adsymptotic.com https://www.google.com https://px.ads.linkedin.com https://*.facebook.com https://*.google-analytics.com http://*.google-analytics.com https://*.swagger.io https://optimize.google.com;";
+            $csp .= " img-src $knownDomainsInsecure 'self' data: https://shielded.co.nz https://googleads.g.doubleclick.net https://stats.g.doubleclick.net https://www.google.co.nz https://p.adsymptotic.com https://www.google.com https://px.ads.linkedin.com https://*.facebook.com https://*.google-analytics.com http://*.google-analytics.com https://*.swagger.io https://optimize.google.com;";
             $csp .= " font-src $knownDomains data: https://*.fontawesome.com https://*.typography.com https://maxcdn.bootstrapcdn.com https://fonts.gstatic.com;";
             $csp .= " object-src $knownDomains 'self';";
             $csp .= " frame-src $knownDomainsInsecure 'self' https://www.facebook.com https://staticcdn.co.nz https://bid.g.doubleclick.net https://www.google.com data: https://*.youtube-nocookie.com https://player.vimeo.com https://*.youtube.com https://optimize.google.com https://www.googletagmanager.com/ns.html https://*.hotjar.com;";
             $csp .= " child-src $knownDomains https://*.youtube-nocookie.com https://player.vimeo.com http://player.vimeo.com https://*.youtube.com  https://optimize.google.com https://www.googletagmanager.com/ns.html https://*.hotjar.com;";
-            $csp .= " connect-src $knownDomains https://www.google-analytics.com/;";
+            $csp .= " connect-src $knownDomains https://*.algolia.net https://*.algolianet.com; https://www.google-analytics.com/;";
             $csp .= " form-action $knownDomainsInsecure https://www.facebook.com 'self';";
             // phpcs:enable
 
@@ -128,4 +128,13 @@ class SecuredControllerExtension extends Extension
 
         return false;
     }
+
+    /**
+     * @return string
+     */
+    public function getEnvironment()
+    {
+        return Director::get_environment_type();
+    }
+
 }
