f1d3b941e06de2d5bf285b5524ddfae525d01f66
Will Rossiter <will@fullscreen.io>
2021-03-06 10:03:54 +1300

    Lock gridfieldexport, support html labels

---
+++ src/Extension/EducationFormFieldExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+use SilverStripe\Core\Extension;
+
+class EducationFormFieldExtension extends Extension
+{
+    /**
+     * Adds the ability to store HTML in a FormField label without customising
+     * the form template.
+     */
+    public function htmlizeLabel()
+    {
+        $casting = $this->owner->config()->get('casting');
+        $casting['Title'] = 'HTMLText';
+
+        $this->owner->config()->set('casting', $casting);
+
+        return $this->owner;
+    }
+
+    /**
+     * Remove HTML from readonly fields.
+     */
+    public function onBeforeRender()
+    {
+        if ($this->owner->isReadonly()) {
+            $this->owner->setTitle(strip_tags($this->owner->Title()));
+        }
+    }
+}
