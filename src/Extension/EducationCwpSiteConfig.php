b473c8a52518eab8d02af432d5a261d58d4a98f3
Will Rossiter <will@fullscreen.io>
2021-03-08 15:02:51 +1300

    Remove old accesskeys

--- src/Extension/EducationCwpSiteConfig.php
+++ src/Extension/EducationCwpSiteConfig.php
@@ -2,15 +2,11 @@
 
 namespace Education\Cwp\Extension;
 
-use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Assets\Image;
 use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TreeDropdownField;
-use SilverStripe\Forms\LiteralField;
 use SilverStripe\ORM\DB;
 use SilverStripe\AssetAdmin\Forms\UploadField;
-use Page;
 
 class EducationCwpSiteConfig extends DataExtension
 {
@@ -19,13 +15,6 @@ class EducationCwpSiteConfig extends DataExtension
     ];
 
     private static $has_one = [
-        'AccessKeyForListOfAccessKeys' => Page::class,
-        'AccessKeyForHome' => Page::class,
-        'AccessKeyForSiteMap' => Page::class,
-        'AccessKeyForAboutThisSite' => Page::class,
-        'AccessKeyForContact' => Page::class,
-        'AccessKeyForLinkToUs' => Page::class,
-        'AccessKeyForLegal' => Page::class,
         'DefaultFeaturedImage'    => Image::class,
         'DefaultFeaturedOGImage'  => Image::class,
     ];
@@ -46,16 +35,6 @@ class EducationCwpSiteConfig extends DataExtension
         $featuredImageField = UploadField::create('DefaultFeaturedOGImage',
             'Default Open Graph Image (site-wide)');
         $fields->addFieldToTab('Root.LogosIcons', $featuredImageField, 'Logo');
-
-        $fields->addFieldsToTab('Root.AccessKeys', [
-            TreeDropdownField::create('AccessKeyForListOfAccessKeysID', '‘Accessibility’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForHomeID', '‘Home’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForSiteMapID', '‘Site map’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForAboutThisSiteID', '‘About this site’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForContactID', '‘Contact’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForLinkToUsID', '‘Link to us’ page ', SiteTree::class),
-            TreeDropdownField::create('AccessKeyForLegalID', '‘Legal and privacy’ page ', SiteTree::class),
-        ]);
 
         $fields->removeByName('FacebookURL');
         $fields->removeByName('TwitterUsername');
